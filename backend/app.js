915418c0c4cedef9618fd59ff8348ccd4b3cd10d
Siddheshwar Mohan kadam <O4317@sbilife.co.in>
2025-05-22 11:00:54 +0530

    chnages added

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+// const mongoose = require('mongoose');
+
+// const mongoURI = 'mongodb://admin:password@mongodb:27017/mydb?authSource=admin';
+
+// mongoose.connect(mongoURI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => console.log('MongoDB connected'))
+// .catch((err) => console.error('MongoDB connection error:', err));
+
+
+// Define the port
+const PORT = process.env.PORT || 3000;
+
+// Middleware (Parse JSON requests)
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello, World! 🚀');
+});
+
+
+
+app.get('/api/message', (req, res) => {
+  res.json({ message: 'Hello from MERN backend!' });
+});
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
