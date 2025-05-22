1d18711b1db36e27c8790e31b9e3ff2adbf06ab7
Siddheshwar Mohan kadam <O4317@sbilife.co.in>
2025-05-22 12:15:11 +0530

    changes added in frontend_new

---
+++ frontend_new/src/App.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [msg, setMsg] = useState('');
+
+
+  
+  useEffect(() => {
+    fetch("/api/message")
+      .then(res => res.json())
+      .then(data => setMsg(data.message));
+  }, []);
+
+  return <h1>{msg}</h1>;
+}
+
+export default App;
