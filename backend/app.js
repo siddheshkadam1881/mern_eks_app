bc4d2d79bc1e065f512e83db96fa151b6914bcb4
Siddheshwar Mohan kadam <O4317@sbilife.co.in>
2025-05-22 14:57:38 +0530

    chnages added

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 
 // Define the port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware (Parse JSON requests)
 app.use(express.json());
--- frontend_new/src/App.js
+++ frontend_new/src/App.js
@@ -1,17 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
 import React, { useEffect, useState } from 'react';
 
 function App() {
+
   const [msg, setMsg] = useState('');
 
-
-  
   useEffect(() => {
     fetch("/api/message")
       .then(res => res.json())
       .then(data => setMsg(data.message));
   }, []);
 
-  return <h1>{msg}</h1>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        </p>
+      <h1>EKS fargate demo app</h1>;
+
+      </header>
+
+      <h1>{msg}</h1>;
+
+
+
+    </div>
+  );
 }
 
 export default App;
