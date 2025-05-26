7159c3397f7b6b6bb0561bb698b155f9f5978afe
Siddheshwar Mohan kadam <O4317@sbilife.co.in>
2025-05-26 11:15:31 +0530

    changes in app.js

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,6 @@ app.get('/api/message', (req, res) => {
 
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
